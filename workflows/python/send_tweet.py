f34b3b0205dbe6b3910237e27183bd107bffccf4
Mat Benfield <1188107+MatBenfield@users.noreply.github.com>
2020-08-29 22:14:14 +0100

    Update send_tweet.py

--- workflows/python/send_tweet.py
+++ workflows/python/send_tweet.py
@@ -1,31 +1,19 @@
-
-# importing the module 
-
+# importing the module
 import tweepy 
 
-  
 # personal details 
+consumer_key = ${{ secrets.tw_consumer_key }}
+consumer_secret = ${{ secrets.tw_consumer_secret }}
+access_token = ${{ secrets.tw_access_token }}
+access_token_secret = ${{ secrets.tw_access_secret }}
 
-consumer_key ="xxxxxxxxxxxxxxxx"
-
-consumer_secret ="xxxxxxxxxxxxxxxx"
-
-access_token ="xxxxxxxxxxxxxxxx"
-
-access_token_secret ="xxxxxxxxxxxxxxxx"
-
-  
 # authentication of consumer key and secret 
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-
   
 # authentication of access token and secret 
 auth.set_access_token(access_token, access_token_secret) 
 
 api = tweepy.API(auth) 
 
-  
 # update the status 
-
 api.update_status(status ="Hello Everyone !") 
