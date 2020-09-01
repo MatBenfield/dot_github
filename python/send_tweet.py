d6def09d25c61062c6e1c0b5e3700cb7767a41e3
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-01 21:16:58 +0100

    pylinting
    add pylint errors only and tidy up with some linting

--- python/send_tweet.py
+++ python/send_tweet.py
@@ -1,20 +1,23 @@
+"""
+Some Title
+"""
 # importing the module
-import tweepy
 import os
+import tweepy
 
-# personal details 
+# personal details
 consumer_key = os.getenv('c_key')
 consumer_secret = os.getenv('c_secret')
 access_token = os.getenv('a_token')
 access_token_secret = os.getenv('a_secret')
 
-# authentication of consumer key and secret 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+# authentication of consumer key and secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   
-# authentication of access token and secret 
-auth.set_access_token(access_token, access_token_secret) 
+# authentication of access token and secret
+auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth) 
+api = tweepy.API(auth)
 
-# update the status 
-api.update_status(status ="Up The Chels!") 
+# update the status
+api.update_status(status ="Up The Chels!")
